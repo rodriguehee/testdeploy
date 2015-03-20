4759ab31c1065c43d53a04c2e541a5b8539b2259
voo4deploy <voo4deploy@ca272a53-f35c-4c06-86ee-202130b647b4>
2015-03-20 10:02:58 +0000

    ajout des fichiers tableurs

---
+++ applications/demo2/trunk/modules/mainframe/controllers/IndexController.php
@@ -0,0 +1,42 @@
+<?php
+
+class Mainframe_IndexController extends Core_Library_Controller_MainFrame_Index
+{
+        public function init()
+        {
+                parent::init() ;
+                $sGridUrl = Core_Library_Options::get( 'lib.grid.url' ) ;
+                $themes = explode( ",", "android,arctic,black,blackberry,bootstrap,energyblue,fresh,highcontrast,metro,metrodark,mobile,office,orange,shinyblack,summer,web,windowsphone" ) ;
+                foreach( $themes as $theme ) {
+                        $this->view->headLink()->prependStylesheet( $sGridUrl . 'styles/jqx.' . $theme . '.css' ) ;
+                }
+                $this->view->headLink()->prependStylesheet( $sGridUrl . 'styles/jqx.base.css' ) ;
+                // !!! jquery-1.10.2.min.js est chargé par Bootstrap dans le noyau !!!
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxcore.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxdata.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxdata.export.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxbuttons.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxscrollbar.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxmenu.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxlistbox.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxcombobox.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxdropdownlist.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxdropdownbutton.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxwindow.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxpanel.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxcheckbox.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxnumberinput.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxdatetimeinput.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxcalendar.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.selection.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.columnsresize.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.edit.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.filter.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.sort.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.pager.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.grouping.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.export.js' ) ;
+                $this->view->headScript()->appendFile( $sGridUrl . 'jqxgrid.aggregates.js' ) ;
+        }
+}
