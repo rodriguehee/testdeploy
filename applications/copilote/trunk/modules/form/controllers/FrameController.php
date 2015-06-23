1894d9e5f111ae7f27e7809dbb241874ca330050
rodrigue <rodrigue@ca272a53-f35c-4c06-86ee-202130b647b4>
2015-06-23 09:11:22 +0000

    gestion des suppressions dans les totaux de depenses et de demandes

--- applications/copilote/trunk/modules/form/controllers/FrameController.php
+++ applications/copilote/trunk/modules/form/controllers/FrameController.php
@@ -13,9 +13,55 @@ class Form_FrameController extends Core_Library_Controller_Form_Frame
 			if( $dataset['id'] == "dmnd" ) {
 				foreach( $dataset['rowdata'] as $row ) {
 					$idDemande = $row['id_data'] ;
-					$montant = $this->computeMontantDemande( $idDemande ) ;
-					$this->setMontantDemande( $idDemande, $montant ) ;
+					if( is_numeric( $idDemande ) ) {
+						$montant = $this->computeMontantDemande( $idDemande ) ;
+						$this->setMontantDemande( $idDemande, $montant ) ;
+					}
 				}
+			}
+		}
+	}
+	
+	/**
+	 * @param Core_Library_Event_Context $context
+	 */
+	protected function _delete_delete_beforeDelete( Core_Library_Event_Context $context )
+	{
+		$varset = $context->get( 'sVarsetId' ) ;
+		
+		if ( in_array( $varset, array( "oc", "rh", "dt", "pe", "dmp" ) ) ) {
+			$demandes = array() ;
+			foreach( $context->get( 'aRecordsIds' )as $id ) {
+				$idDemande = $this->getIdDemande( $varset, $id ) ;
+				if ( ! in_array( $idDemande, $demandes ) ) {
+					$demandes[] = $idDemande ;
+				}
+			}
+			$context->add( "demandes", $demandes ) ;
+		}
+		elseif ( "ventilation" == $varset ) {
+			$demandes = array() ;
+			foreach( $context->get( 'aRecordsIds' )as $id ) {
+				$idDemande = $this->getIdDemandeFromVentilation( $id ) ;
+				if ( ! in_array( $idDemande, $demandes ) ) {
+					$demandes[] = $idDemande ;
+				}
+			}
+			$context->add( "demandes", $demandes ) ;
+		}
+	}
+	
+	/**
+	 * @param Core_Library_Event_Context $context
+	 */
+	protected function _delete_delete_afterDelete( Core_Library_Event_Context $context )
+	{
+		$varset = $context->get( 'sVarsetId' ) ;
+	
+		if ( in_array( $varset, array( "oc", "rh", "dt", "pe", "dmp", "ventilation" ) ) ) {
+			foreach( $context->get( 'demandes' )as $idDemande ) {
+				$montant = $this->computeMontantDemande( $idDemande ) ;
+				$this->setMontantDemande( $idDemande, $montant ) ;
 			}
 		}
 	}
@@ -68,6 +114,7 @@ class Form_FrameController extends Core_Library_Controller_Form_Frame
 			"id_oc" => "oc",
 			"id_pe" => "pe",
 			"id_dmp" => "dmp",
+			"id_rh" => "rh",
 		) ;
 		foreach( $ventilations as $foreignKey => $varsetPrefix ) {
 			$query = sprintf( "
@@ -112,6 +159,48 @@ class Form_FrameController extends Core_Library_Controller_Form_Frame
 		$db->setFetchMode( Zend_Db::FETCH_OBJ ) ;
 		$sql = 'SELECT id_data FROM cplt_dpns_data WHERE id_demande = ?' ;
 		$result = $db->fetchCol( $sql, $idDemande ) ;
+		return $result ;
+	}
+	
+	/**
+	 * @return integer
+	 * @param integer $id
+	 */
+	public function getIdDemandeFromVentilation( $id )
+	{
+		$db = Core_Library_Account::GetInstance()->GetCurrentProject()->Db() ;
+		$sql = '
+			SELECT id_oc as oc, id_pe as pe, id_dmp as dmp, id_rh as rh
+			FROM cplt_vntl_data v
+			WHERE v.id_data = ?
+		' ;
+		$result = $db->fetchAll( $sql, $id ) ;
+		$row = $result[0] ;
+		foreach( $row as $key => $value ) {
+			if( ! empty( $value ) ) {
+				return $this->getIdDemande( $key, $value ) ;
+			}
+		}
+		throw new LogicException( sprintf( "Ventilation '%s' orpheline !!!", $id ) ) ;
+	}
+		
+	/**
+	 * @return integer
+	 * @param string $varset
+	 * @param integer $id
+	 */
+	public function getIdDemande( $varset, $id )
+	{
+		$db = Core_Library_Account::GetInstance()->GetCurrentProject()->Db() ;
+		$sql = sprintf( '
+			SELECT d.id_demande 
+			FROM cplt_dpns_data d
+			JOIN cplt_%s_data t
+			  ON d.id_data = t.id_depense
+			WHERE t.id_data = ?
+		', 	$varset
+		) ;
+		$result = (int) $db->fetchOne( $sql, $id ) ;
 		return $result ;
 	}
 }
