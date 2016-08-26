6c95da30265be4b38390104a9b47e146b6e42f23
rodrigue <rodrigue@ca272a53-f35c-4c06-86ee-202130b647b4>
2016-08-26 08:39:18 +0000

    Module de programmation pluriannuelle

--- applications/copilote/trunk/library/Demande.php
+++ applications/copilote/trunk/library/Demande.php
@@ -13,6 +13,11 @@ class Copilote_Library_Demande extends Copilote_Library_Record
 	protected $_ub ;
 	
 	/**
+	 * @var Copilote_Library_Record
+	 */
+	protected $_sb ;
+	
+	/**
 	 * @param string $tableName
 	 * @param integer $idData
 	 */
@@ -63,5 +68,158 @@ class Copilote_Library_Demande extends Copilote_Library_Record
 			$this->_ub = new Copilote_Library_Record( "cplt_ub_data", $id ) ;
 		}
 		return $this->_ub ;
+	}
+	
+	/**
+	 * @return float
+	 * @param string $nature "personnel" | "fonctionnement" | "investissement"
+	 * @param string $typeDepense "rh" | "dt" | "sta" | "oc" | "pe" | "dmp" 
+	 * @param string $aspect "ae" | "cp"
+	 * @param string $imputation "scsp" | "convention"
+	 */
+	public function getMontant( $nature, $typeDepense, $aspect, $imputation )
+	{
+		$montant = 0.0 ;
+		
+		foreach( $this->_depenses as $depense ) {
+			assert( $depense instanceof Copilote_Library_Depense ) ;
+			$montant += $depense->getMontant( $nature, $typeDepense, $aspect, $imputation ) ;
+		}
+		
+		return $montant ;
+	}
+	
+	/**
+	 * @return float
+	 * @param Copilote_Library_Convention $convention
+	 */
+	public function GetMontantPersonnel( Copilote_Library_Convention $convention )
+	{
+		$fMontant = 0.0 ;
+		$db = Core_Library_Account::GetInstance()->GetCurrentProject()->Db() ;
+		
+		foreach( range( 1, 2 ) as $index ) {
+			$query = sprintf( "
+				SELECT SUM(rh_impc%dtot) 
+				FROM cplt_rh_data rh
+				JOIN cplt_dpns_data dp ON rh.id_depense = dp.id_data
+				WHERE dp.id_demande = %d 
+				  AND rh.rh_impc%d = %d
+			", 	$index, 
+				$this->getId(),
+				$index,
+				$convention->getId()
+			) ;
+			$row = $db->fetchRow( $query ) ;
+			if( ! empty( $row ) ) {
+				$fMontant += (float) current( $row ) ;
+			}
+		}
+		
+		return $fMontant ;
+	}
+	
+	/**
+	 * @return float
+	 * @param Copilote_Library_Convention $convention
+	 * @param string $nature "fonctionnement" | "investissement"
+	 * @param string $aspect "ae" | "cp"
+	 */
+	public function GetAutreMontant( Copilote_Library_Convention $convention, $nature, $aspect )
+	{
+		$fMontant = 0.0 ;
+		$db = Core_Library_Account::GetInstance()->GetCurrentProject()->Db() ;
+		
+		$invest = 10 ;
+		if( "fonctionnement" == $nature ) {
+			$invest = 11 ;
+		}
+		
+		$field = "cout" ;
+		if( "cp" == $aspect ) {
+			$field = "cout_cp" ;
+		}
+		
+		$prefixes = array( "pe", "dmp" ) ;
+		if( $nature == "fonctionnement" && $aspect == "ae" ) {
+			$prefixes[] = "oc" ;
+		}
+		
+		// ventilations
+		foreach( $prefixes as $prefix ) {
+			$query = sprintf( "
+				SELECT SUM(v.%s) 
+				FROM cplt_dpns_data dp
+				JOIN cplt_%s_data t ON dp.id_data = t.id_depense
+				JOIN cplt_vntl_data v ON t.id_data = v.id_%s 
+				WHERE v.invest = %d 
+				  AND dp.id_demande = %d
+				  AND v.convention = %d
+			",  $field, 
+				$prefix,
+				$prefix,
+				$invest,
+				$this->getId(),
+				$convention->getId() 
+			) ;
+			$row = $db->fetchRow( $query ) ;
+			if( ! empty( $row ) ) {
+				$fMontant += (float) current( $row ) ;
+			}
+		}
+		
+		if( $nature == "fonctionnement" && $aspect == "ae" ) {
+			// sta 
+			foreach( range( 1, 2 ) as $index ) {
+				$query = sprintf( "
+					SELECT SUM(sta.sta_impc%dtot)
+					FROM cplt_dpns_data dp
+					JOIN cplt_sta_data sta 
+					  ON dp.id_data = sta.id_depense
+					WHERE dp.id_demande = %d
+					  AND sta.sta_impc%d = %d
+				",  $index, 
+					$this->getId(), 
+					$index,
+					$convention->getId()
+				) ;
+				$row = $db->fetchRow( $query ) ;
+				if( ! empty( $row ) ) {
+					$fMontant += (float) current( $row ) ;
+				}
+			}
+			
+			// dt 
+			$query = sprintf( "
+				SELECT SUM(dt.dt_couttot)
+				FROM cplt_dpns_data dp
+				JOIN cplt_dt_data dt 
+				  ON dp.id_data = dt.id_depense
+				WHERE dp.id_demande = %d
+				  AND dt.dt_numconv = %d
+			",  $this->getId(),
+				$convention->getId()
+			) ;
+			$row = $db->fetchRow( $query ) ;
+			if( ! empty( $row ) ) {
+				$fMontant += (float) current( $row ) ;
+			}
+		}
+		
+		return $fMontant ;
+	}
+	
+	/**
+	 * @return Copilote_Library_SuiviBudgetaire
+	 */
+	public function GetSuiviBudgetaire()
+	{
+		if( empty( $this->_sb ) ) {
+			$db = Core_Library_Account::GetInstance()->GetCurrentProject()->Db() ;
+			$sql = 'SELECT id_data FROM cplt_sub_data WHERE id_demande = ?' ;
+			$id = (int) $db->fetchOne( $sql, $this->getId() ) ;
+			$this->_sb = new Copilote_Library_Record( "cplt_sub_data", $id ) ;
+		}
+		return $this->_sb ;
 	}
 }
